e9115d89886a7a24e36f62dcea4bfa878f2abd69
unknown <>
2016-01-20 18:52:26 +0800

    新增安装初始设置。

--- xy_import_callback.php
+++ xy_import_callback.php
@@ -1,6 +1,8 @@
 <?php
 function callback_install() {
 	option::set('xy_import_gs','{百度ID}----BDUSS={百度BDUSS}');
+	option::set('xy_import_check',1);
+	option::set('xy_import_refresh',1);
 }
 
 function callback_remove() {
