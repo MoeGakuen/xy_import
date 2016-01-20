ba3e36c8d8f1264800ceb79148c73b9013788959
unknown <>
2016-01-20 18:51:35 +0800

    优化菜单文本。

--- xy_import.php
+++ xy_import.php
@@ -4,7 +4,7 @@ if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); }
 function xy_import_navi() { 
 	echo '<li ';
 	if(isset($_GET['plug']) && $_GET['plug'] == 'xy_import') { echo 'class="active"'; }
-	echo '><a href="index.php?mod=admin:setplug&plug=xy_import"><span class="glyphicon glyphicon-import"></span> 批量导入BDUSS</a></li>';
+	echo '><a href="index.php?mod=admin:setplug&plug=xy_import"><span class="glyphicon glyphicon-import"></span> 批量导入</a></li>';
 }
 
 addAction('navi_2','xy_import_navi');
