269682e59d38e4900b6413ce198a5385a59416e0
unknown <>
2016-01-25 11:32:55 +0800

    增加批量导入前格式验证、优化自定义匹配逻辑、修复v1.1版本检测BDUSS功能失效

--- view_ajax.php
+++ view_ajax.php
@@ -1,41 +1,54 @@
 <?php
-ini_set("max_execution_time", 300);
-if(ROLE != 'admin') die('权限不足');
+ini_set("max_execution_time", 0);
+if(ROLE != 'admin') die('{"type":"error","emsg":"权限不足！"}');
 global $m;
 //参数替换
-function getgs($gs){
+function getgs($gs,$type=false){
 	$data = str_ireplace('{百度ID}','(.*)',$gs);
 	$data = str_ireplace('{百度BDUSS}','([0-9a-zA-Z\-\~]+)',$data);
-	return $data; 
+	if($type) {
+		global $re_a,$re_b;
+		$re_a = stripos($gs,'{百度ID}');
+		$re_b = stripos($gs,'{百度BDUSS}');
+		if ($re_a > $re_b) {
+			$re_a = 2;$re_b = 1;
+		} else {
+			$re_a = 1;$re_b = 2;
+		}
+	}
+	return $data;
 }
 //导入BDUSS
 if (isset($_GET['new'])) {
   $import_str = !empty($_POST['import_str']) ? $_POST['import_str'] : '';
-  if(empty($import_str)) ReDirect(SYSTEM_URL.'index.php?mod=admin:setplug&plug=xy_import&error_msg=导入文本不能为空！');
+  if(empty($import_str)) die('{"type":"error","emsg":"导入文本不能为空！"}');
+  $gs = option::get('xy_import_gs');
+  if(stripos($gs,'{百度ID}')===FALSE) die('{"type":"error","emsg":"导入格式缺少参数 <strong>{百度ID}</strong> ！"}');
+  elseif(stripos($gs,'{百度BDUSS}')===FALSE) die('{"type":"error","emsg":"导入格式缺少参数 <strong>{百度BDUSS}</strong> ！"}');
+  $check = option::get('xy_import_check') == 1 ? true : false;
   $import_str = preg_replace('/[\r\n]+/', PHP_EOL, $import_str);
   $arr = explode(PHP_EOL,$import_str);
   $total = count($arr);
-  $gs = option::get('xy_import_gs');
   $refresh = option::get('xy_import_refresh');
-  $hs=$cf=$ok=$err=$up=$sx=0;
+  $hs=$cf=$ok=$err=$up=$sx=$re_a=$re_b=0;
   for($i=0;$i<$total;$i++){
-	preg_match('/'.getgs($gs).'/',$arr[$i], $re);
-	if (!empty($re[2])) {
+	preg_match('/'.getgs($gs,true).'/',$arr[$i], $re);
+	if (!empty($re[$re_b])) {
 	  $hs++;
-	  $x = $m->once_fetch_array("SELECT COUNT(*) AS bduss FROM `".DB_NAME."`.`".DB_PREFIX."baiduid` where `bduss` = '".$re[2]."';");
+	  $x = $m->once_fetch_array("SELECT COUNT(*) AS bduss FROM `".DB_NAME."`.`".DB_PREFIX."baiduid` where `bduss` = '".$re[$re_b]."';");
 	  if ($x['bduss'] > 0) {
 		$cf++;
 	  } else {
-		$baidu_name = option::get('xy_import_check') == 1 ? sqladds(getBaiduId($re[2])) : sqladds($re[1]);
+		$baidu_name = $check ? sqladds(getBaiduId($re[$re_b])) : sqladds($re[$re_a]);
 		if(empty($baidu_name)){
 		  $sx++;
 		} else {
 		  $z = $m->once_fetch_array("SELECT COUNT(*) AS bdname FROM `".DB_NAME."`.`".DB_PREFIX."baiduid` where `name` = '".$baidu_name."';");
 		  if ($z['bdname'] > 0) {
-			$sql = "UPDATE `".DB_NAME."`.`".DB_PREFIX."baiduid` SET `uid`='".UID."', `bduss`='".$re[2]."' where `name`='".$baidu_name."';";
+			$sql = "UPDATE `".DB_NAME."`.`".DB_PREFIX."baiduid` SET `uid`='".UID."', `bduss`='".$re[$re_b]."' where `name`='".$baidu_name."';";
 			$m->query($sql) ? $up++ : $err++;
 		  } else {
-			$sql = "INSERT INTO `".DB_NAME."`.`".DB_PREFIX."baiduid` (`uid`, `bduss`, `name`) VALUES ('".UID."', '".$re[2]."', '".$baidu_name."');";
+			$sql = "INSERT INTO `".DB_NAME."`.`".DB_PREFIX."baiduid` (`uid`, `bduss`, `name`) VALUES ('".UID."', '".$re[$re_b]."', '".$baidu_name."');";
 			$m->query($sql) ? $ok++ : $err++;
 		  }
 		}
